7ad6e3f467cdcecaa6d2dee4ca69697fab238398
Barto <juanmartin.cerruti@gmail.com>
2021-12-23 13:28:37 -0300

    add arroiws

---
+++ src/components/ProductSlider.vue
@@ -0,0 +1,118 @@
+<template>
+  <div class="ProductSlider">
+    <div class="product-slider">
+      <div class="glide__track" data-glide-el="track">
+        <ul class="glide__slides">
+          <li v-for="(diapo, i) in diapos" :key="i">
+            <div
+              v-if="i == 0"
+              class="main-product-diapo"
+              :style="buildMainImage(diapo.image)"
+            >
+              <div class="logo" :style="buildLogo(diapo.logo)" />
+            </div>
+
+            <div v-if="i != 0" class="product-diapo">
+              <img :src="diapo.image" />
+              <p>{{ diapo.description }}</p>
+            </div>
+          </li>
+        </ul>
+      </div>
+      <div class="glide__arrows" data-glide-el="controls">
+        <button class="glide__arrow glide__arrow--left" data-glide-dir="<">
+          A
+        </button>
+        <button class="glide__arrow glide__arrow--right" data-glide-dir=">">
+          {{">"}}
+        </button>
+      </div>
+      <div class="container">
+        <div class="glide__bullets" data-glide-el="controls[nav]">
+          <li v-for="(_, i) in diapos" :key="i">
+            <p class="f-Noe-bold f-80 f-mobile-46 lh-80 c-white">
+              {{ i }}
+            </p>
+          </li>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+const productSlider = {
+  id: 0,
+  brand: "",
+  logo: "",
+  image: "",
+  items: [
+    { image: "", name: "", description: "", price: "" },
+    { image: "", name: "", description: "", price: "" },
+    { image: "", name: "", description: "", price: "" },
+    { image: "", name: "", description: "", price: "" },
+    { image: "", name: "", description: "", price: "" },
+  ],
+};
+
+const diapos = [
+  { logo: productSlider.logo, image: productSlider.image },
+  ...productSlider.items,
+];
+
+console.log("diapos", diapos);
+
+export default {
+  name: "ProductsSection",
+  props: {
+    id: String,
+    // items: Product[],
+  },
+  data: () => ({
+    diapos,
+  }),
+  methods: {
+    buildLogo: (logo) => {
+      return `backgroundImage: url${logo}`;
+    },
+    buildMainImage: (image) => {
+      return `backgroundImage: url${image}`;
+    },
+  },
+  computed: {},
+};
+</script>
+
+<style scoped>
+.ProductSlider {
+  position: relative;
+  height: 300px;
+  width: 200px;
+  background-color: grey;
+}
+
+.ProductSlider ul li {
+  width: 100%;
+}
+.ProductSlider .main-product-diapo {
+  width: 100%;
+  height: 300px;
+  position: relative;
+  background: green;
+}
+.ProductSlider .main-product-diapo .logo {
+  width: 50px;
+  height: 50px;
+  background: white;
+  opacity: 0.6;
+  position: absolute;
+  left: 15px;
+  top: 15px;
+}
+.ProductSlider .product-diapo {
+  width: 100%;
+  height: 300px;
+  position: relative;
+  background: red;
+}
+</style>
---
+++ src/components/ProductsSection.vue
@@ -0,0 +1,115 @@
+<template>
+  <div class="ProductsSection">
+    <div class="row">
+      <div>
+        <ProductSlider id="1" />
+      </div>
+      <div class="upper">
+        <ProductSlider id="2" />
+      </div>
+      <div>
+        <ProductSlider id="3" />
+      </div>
+      <div class="upper">
+        <ProductSlider id="4" />
+      </div>
+      <div>
+        <ProductSlider id="5" />
+      </div>
+    </div>
+    <div class="row row-bot">
+      <div class="upper">
+        <ProductSlider id="6" />
+      </div>
+      <div>
+        <ProductSlider id="7" />
+      </div>
+      <div class="upper">
+        <ProductSlider id="8" />
+      </div>
+      <div>
+        <ProductSlider id="9" />
+      </div>
+      <div class="upper">
+        <ProductSlider id="10" />
+      </div>
+    </div>
+    <script type="application/javascript" v-html="this.renderStatic()" />
+  </div>
+</template>
+
+<script>
+import ProductSlider from "./ProductSlider.vue";
+
+const staticDomElement = () => {
+  let glideJSScript = document.createElement("script");
+  glideJSScript.setAttribute(
+    "src",
+    "https://cdnjs.cloudflare.com/ajax/libs/Glide.js/3.2.0/glide.min.js"
+  );
+  document.head.appendChild(glideJSScript);
+  glideJSScript.onload = () => {
+    /* necesario */
+    document.querySelectorAll(".product-slider").forEach((i) => {
+      new window.Glide(i, {
+        peek: 0,
+        perView: 1,
+        type: "carrusel",
+        autoplay: false,
+        gap: 0,
+      }).mount();
+    });
+    /* necesario */
+  };
+};
+export default {
+  name: "ProductsSection",
+  components: {
+    ProductSlider,
+  },
+  props: {
+    // items: Product[],
+  },
+  data: () => ({}),
+  methods: {
+    renderStatic: () => {
+      return `
+        ${staticDomElement.toString()}
+        staticDomElement();
+      `;
+    },
+  },
+  mounted() {
+    //staticDomElement()
+  },
+  computed: {},
+};
+</script>
+
+<style scoped>
+.ProductsSection {
+  position: relative;
+  margin-bottom: 100px;
+  max-width: 1050px;
+    margin: auto;
+}
+.ProductsSection .row {
+  display: flex;
+  justify-content: space-between;
+}
+
+.ProductsSection .row div:not(.upper) {
+  margin-top: 0px;
+  margin-bottom: -0px;
+}
+
+.ProductsSection .row-bot div.upper {
+  margin-top: -20px;
+  margin-bottom: -20px;
+}
+
+.ProductsSection .row .upper {
+  margin-bottom: 20px;
+  margin-top: 20px;
+}
+</style>
